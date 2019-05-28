62d13258fe2706707919cc66c76084a88e21e3ad
clementclementson <cjclementson@gmail.com>
2019-05-28 22:46:50 +0200

    Renamed files and split sorting into it's own file

---
+++ Python/Sorting/bubble_sort.py
@@ -0,0 +1,48 @@
+import random
+import matplotlib
+import matplotlib.pyplot as plt
+
+def random_data(range_values):
+    array = []
+
+    for index in range(range_values):
+        array.append(random.randint(0, 100))
+
+    return array
+
+def linear_data(range_values):
+    array = []
+    i = range_values
+
+    while i > 0:
+        array.append(i)
+        i = i -1
+
+    return array
+
+def bubble_sort(range_values):
+    xarray = []
+
+    for index in range(range_values):
+        xarray.append(index)
+
+    array = random_data(range_values)
+
+    for i in range(len(array) - 1):
+        for j in range(0, (len(array) - i - 1)):
+            if array[j] > array[j+1]:
+                temp = array[j]
+                array[j] = array[j+1]
+                array[j+1] = temp
+                
+                plt.cla()
+                plt.plot(xarray, array, color = 'blue')	
+                plt.scatter(xarray[j], array[j], color = 'red')
+                plt.scatter(xarray[j+1], array[j+1], color = 'red')
+                plt.pause(0.0001)
+
+    plt.plot(xarray, array)
+    plt.show()
+
+range_values = 100
+bubble_sort(range_values)
--- Python/Sorting/selective_sort.py
+++ Python/Sorting/selective_sort.py
@@ -51,29 +51,5 @@ def selective_sort(range_values):
     plt.plot(xarray, array)
     plt.show()
 
-def bubble_sort(range_values):
-    xarray = []
-
-    for index in range(range_values):
-        xarray.append(index)
-
-    array = random_data(range_values)
-
-    for i in range(len(array) - 1):
-        for j in range(0, (len(array) - i - 1)):
-            if array[j] > array[j+1]:
-                temp = array[j]
-                array[j] = array[j+1]
-                array[j+1] = temp
-                
-                plt.cla()
-                plt.plot(xarray, array, color = 'blue')	
-                plt.scatter(xarray[j], array[j], color = 'red')
-                plt.scatter(xarray[j+1], array[j+1], color = 'red')
-                plt.pause(0.0001)
-
-    plt.plot(xarray, array)
-    plt.show()
-
 range_values = 100
-bubble_sort(range_values)
+selective_sort(range_values)
